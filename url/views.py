56059e90b2b40cbe0811c113558ffb9fc59049c2
Tahseen Rahman <tahseen.rahman@saarthi.ai>
2019-09-21 00:15:35 +0530

    Error handling & bootstrap

--- url/views.py
+++ url/views.py
@@ -7,8 +7,15 @@ def index(request):
     if request.method == "POST":
         og_url = request.POST.get("og_url")
         short_url = request.POST.get("short_url")
-        url_db(og=og_url, short=short_url).save()
-        return HttpResponse("Your new URL is " + short_url)
+        try:
+            url_db(og=og_url, short=short_url).save()
+        except:
+            msg = "This desired URL already exists, please try a new one!"
+            context = {"og_url": og_url, "msg": msg}
+            return render(request, 'index.html', context)
+        url = request.build_absolute_uri() + short_url
+        context = {'url': url}
+        return render(request, 'index.html', context)
     else:
         return render(request, 'index.html')
 
