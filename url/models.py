c7c345bf3fe1a6eb43c280187de5e6a49d9fba14
Tahseen Rahman <tahseen.rahman@saarthi.ai>
2019-09-22 21:59:36 +0530

    generating random url

--- url/models.py
+++ url/models.py
@@ -5,4 +5,7 @@ from django.db import models
 
 class url_db(models.Model):
     og = models.URLField()
-    short = models.CharField(max_length=10, unique=True)
+    short = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.short
--- url/views.py
+++ url/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import url_db
+import shortuuid
 
 
 def index(request):
@@ -8,9 +9,11 @@ def index(request):
         og_url = request.POST.get("og_url")
         short_url = request.POST.get("short_url")
         try:
+            if short_url == "":
+                short_url = shortuuid.ShortUUID().random(length=5)
             url_db(og=og_url, short=short_url).save()
         except:
-            msg = "This desired URL already exists, please try a new one!"
+            msg = "This desired URL already exists or maybe your short URL is wayyyyyyy too long, please try a new one!"
             context = {"og_url": og_url, "msg": msg}
             return render(request, 'index.html', context)
         url = request.build_absolute_uri() + short_url
